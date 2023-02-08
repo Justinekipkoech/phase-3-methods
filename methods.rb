29c4793003361565bd8c6b0f47d618fa6b09df22
justinekipkoech <justinekipkoech98@gmail.com>
2023-02-08 21:40:04 +0300

    commit tree e227de364b250d5d02cbc48bb4b9bda76e892e51

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
-# Your code here!
+def greet_programmer
+    puts"Hello, programmer!"
+end
+greet_programmer
+
+
+# def greet(name="Naureen")
+#     puts"Hello,#{name}!"
+# end
+
+# greet()
+# greet("Jimmy")
+
+
+def greet_with_default(name="programmer")
+    puts"Hello,#{name}!"
+end
+greet_with_default
+
+
+def add(num1,num2)
+    return num1+num2
+end
+# add(2,2)
+
+
+def halve(number)
+    if type of number !="number"
+        return nil
+    end
+    return number/2
+end
+halve
