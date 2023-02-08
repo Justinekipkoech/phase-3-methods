c3466cc89f9a7d0e26ca9f7bd610171e8f1d4476
justinekipkoech <justinekipkoech98@gmail.com>
2023-02-08 21:54:16 +0300

    completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
-# Your code here!
+def greet_programmer
+    puts"Hello, programmer!"
+end
+greet_programmer
+
+
+def greet(name="Naureen")
+    puts"Hello, #{name}!"
+end
+
+
+
+def greet_with_default(name="programmer")
+    puts"Hello, #{name}!"
+end
+
+
+def add(num1,num2)
+    return num1+num2
+end
+# # add(2,2)
+
+
+def halve(number)
+    if number !=6
+        return nil
+    end
+    return number/2
+end
+# halve
+
